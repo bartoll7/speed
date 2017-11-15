6b9881ab8fc2f270cf5149a8ac6dee9c99f76aee
Bartosz Walaszczyk <b.walaszczyk@tvn.pl>
2017-11-15 21:05:26 +0100

    add controller and render data in view

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,13 +9,14 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/avg-speed", name="avg")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        $result = $this->get('app.avg_view.factory')->createCollection();
+
+        return $this->render('base.html.twig', array(
+            'result' => $result,
+        ));
     }
 }
